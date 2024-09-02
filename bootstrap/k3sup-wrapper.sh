9816eb6ad8e7f10fa2935e23c7fac91023a5676a
ognyan-lazarov-cloudoffice <ognyan.lazarov@cloudoffice.bg>
2024-09-02 15:27:47 +0300

    Transformed k3s-config.yaml to config.yaml and edited k3s-wrapper.sh respectively

--- bootstrap/k3sup-wrapper.sh
+++ bootstrap/k3sup-wrapper.sh
@@ -4,8 +4,9 @@ set -e
 # Configuration
 K3S_VERSION="v1.30.4+k3s1"
 SERVER_IP="192.168.65.6"
-# AGENT_IPS="192.168.65.7"
+AGENT_IPS="192.168.65.7"
 # AGENT_IPS="IP1 IP2 IP3"
+CONFIG_FILE="/path/to/config.yaml"
 
 # Install k3sup if not present
 if ! command -v k3sup &> /dev/null; then
@@ -14,11 +15,17 @@ if ! command -v k3sup &> /dev/null; then
 fi
 
 # Install K3s server
-k3sup install --ip $SERVER_IP --k3s-version $K3S_VERSION
+k3sup install \
+	--ip $SERVER_IP \
+	--k3s-version $K3S_VERSION \
+	--k3s-extra-args="--config ${CONFIG_FILE}"
 
 # Install K3s agents
 for AGENT_IP in $AGENT_IPS; do
-    k3sup join --ip $AGENT_IP --server-ip $SERVER_IP --k3s-version $K3S_VERSION
+  k3sup join \
+		--ip $AGENT_IP \
+		--server-ip $SERVER_IP \
+		--k3s-version $K3S_VERSION
 done
 
 # Apply K3s configuration
