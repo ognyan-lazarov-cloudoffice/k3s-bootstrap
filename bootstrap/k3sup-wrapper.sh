50434a29f3f37e13c0283e2cf488e3a0b9b4b21d
ognyan-lazarov-cloudoffice <ognyan.lazarov@cloudoffice.bg>
2024-09-02 15:51:47 +0300

    Added --ssh-key and --user flags to k3sup install/join commands in k3sup-wrapper.sh

--- bootstrap/k3sup-wrapper.sh
+++ bootstrap/k3sup-wrapper.sh
@@ -6,6 +6,7 @@ K3S_VERSION="v1.30.4+k3s1"
 SERVER_IP="192.168.65.6"
 AGENT_IPS="192.168.65.7"
 # AGENT_IPS="IP1 IP2 IP3"
+USER=sentian
 CONFIG_FILE="/path/to/config.yaml"
 
 # Install k3sup if not present
@@ -18,15 +19,19 @@ fi
 k3sup install \
 	--ip $SERVER_IP \
 	--k3s-version $K3S_VERSION \
-	--k3s-extra-args="--config ${CONFIG_FILE}"
+	--k3s-extra-args="--config ${CONFIG_FILE}" \
+	--ssh-key $HOME/.ssh/id_ed25519 \
+	--user $USER
 
 # Install K3s agents
 for AGENT_IP in $AGENT_IPS; do
   k3sup join \
 		--ip $AGENT_IP \
 		--server-ip $SERVER_IP \
-		--k3s-version $K3S_VERSION
+		--k3s-version $K3S_VERSION \
+		--ssh-key $HOME/.ssh/id_ed25519 \
+		--user $USER
 done
 
 # Apply K3s configuration
-kubectl apply -f k3s-config.yaml
+# kubectl apply -f k3s-config.yaml
